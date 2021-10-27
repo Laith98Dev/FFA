9d605b27db901f8d83d88098a42cfa46c012448c
Laith Youtuber <49840784+Laith98Dev@users.noreply.github.com>
2021-10-27 07:56:01 -0700

    Create ArenasTask.php

---
+++ src/Laith98Dev/FFA/ArenasTask.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Laith98Dev\FFA;
+
+/*  
+ *  A plugin for PocketMine-MP.
+ *  
+ *	 _           _ _   _    ___   ___  _____             
+ *	| |         (_) | | |  / _ \ / _ \|  __ \            
+ *	| |     __ _ _| |_| |_| (_) | (_) | |  | | _____   __
+ *	| |    / _` | | __| '_ \__, |> _ <| |  | |/ _ \ \ / /
+ *	| |___| (_| | | |_| | | |/ /| (_) | |__| |  __/\ V / 
+ *	|______\__,_|_|\__|_| |_/_/  \___/|_____/ \___| \_/  
+ *	
+ *	Copyright (C) 2021 Laith98Dev
+ *  
+ *	Youtube: Laith Youtuber
+ *	Discord: Laith98Dev#0695
+ *	Gihhub: Laith98Dev
+ *	Email: help@laithdev.tk
+ *	Donate: https://paypal.me/Laith113
+ *
+ *	This program is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	This program is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 	
+ */
+
+use pocketmine\scheduler\Task;
+
+class ArenasTask extends Task {
+	
+	/** @var Main */
+	private $plugin;
+	
+	public function __construct(Main $plugin){
+		$this->plugin = $plugin;
+	}
+	
+	public function onRun(int $tick){
+		foreach ($this->plugin->getArenas() as $arena){
+			$arena->tick();
+		}
+	}
+}
